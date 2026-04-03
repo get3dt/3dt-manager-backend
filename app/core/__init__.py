6310d287e52a0a619fb52a9644f76e387b88b9f6
buguno <brunnodesouzabezerra@gmail.com>
2026-04-02 22:52:20 -0300

    feat(init): adding security functions

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,1 +1,4 @@
+from .security import create_access_token as create_access_token
+from .security import get_password_hash as get_password_hash
+from .security import verify_password as verify_password
 from .settings import Settings as Settings
