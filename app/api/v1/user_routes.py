cf2e107530e98d97fade40ffb081e397f7083346
buguno <brunnodesouzabezerra@gmail.com>
2026-04-07 13:01:52 -0300

    feat(user_routes): adding annotated typing and applying auth with jwt in some user endpoints

--- app/api/v1/user_routes.py
+++ app/api/v1/user_routes.py
@@ -1,13 +1,13 @@
 from http import HTTPStatus
-from typing import List
+from typing import Annotated, List
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
 
 from app.core.security import get_current_user
 from app.db.database import get_session
 from app.models import User
-from app.schemas import UserRequest, UserResponse
+from app.schemas import FilterPage, UserRequest, UserResponse
 from app.schemas.user_schema import Message
 from app.services import (
     create_user_service,
@@ -18,25 +18,30 @@ from app.services import (
 
 router = APIRouter(prefix='/users', tags=['users'])
 
+Session = Annotated[Session, Depends(get_session)]
+CurrentUser = Annotated[User, Depends(get_current_user)]
+
 
 @router.post('/', status_code=HTTPStatus.CREATED, response_model=UserResponse)
-def create_user(user: UserRequest, session: Session = Depends(get_session)):
+def create_user(user: UserRequest, session: Session):
     return create_user_service(user, session)
 
 
 @router.get('/', response_model=List[UserResponse])
 def get_users(
-    skip: int = 0, limit: int = 100, session: Session = Depends(get_session)
+    session: Session,
+    filter_users: Annotated[FilterPage, Query()],
+    current_user: CurrentUser,
 ):
-    return get_users_service(session, skip, limit)
+    return get_users_service(session, filter_users)
 
 
 @router.put('/{user_id}', response_model=UserResponse)
 def update_user(
     user_id: int,
     user: UserRequest,
-    session: Session = Depends(get_session),
-    current_user: User = Depends(get_current_user),
+    session: Session,
+    current_user: CurrentUser,
 ):
     if current_user.id != user_id:
         raise HTTPException(
@@ -49,8 +54,8 @@ def update_user(
 @router.delete('/{user_id}', response_model=Message)
 def delete_user(
     user_id: int,
-    session: Session = Depends(get_session),
-    current_user: User = Depends(get_current_user),
+    session: Session,
+    current_user: CurrentUser,
 ):
     if current_user.id != user_id:
         raise HTTPException(
