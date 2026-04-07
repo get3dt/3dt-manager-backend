9ba66f87ab37254135edd968acce8cb3e50d2057
buguno <brunnodesouzabezerra@gmail.com>
2026-04-07 13:03:57 -0300

    feat(filter_page_schema): adding filter page with offset and limit on schema

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,3 +1,4 @@
+from .filter_page_schema import FilterPage as FilterPage
 from .token_schema import Token as Token
 from .user_schema import UserRequest as UserRequest
 from .user_schema import UserResponse as UserResponse
