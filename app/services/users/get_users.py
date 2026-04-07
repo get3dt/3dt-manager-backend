2014a42651c148016a762adae1d05ebeb6d74b6e
buguno <brunnodesouzabezerra@gmail.com>
2026-04-07 13:04:23 -0300

    refactor(get_users): changing skip by offset in service

--- app/services/users/get_users.py
+++ app/services/users/get_users.py
@@ -4,9 +4,8 @@ from sqlalchemy.orm import Session
 
 from app.models import User
 from app.repositories import get_users
+from app.schemas import FilterPage
 
 
-def get_users_service(
-    session: Session, skip: int = 0, limit: int = 100
-) -> List[User]:
-    return get_users(session, skip, limit)
+def get_users_service(session: Session, filter_page: FilterPage) -> List[User]:
+    return get_users(session, filter_page.offset, filter_page.limit)
